4d314df26c3e373f21e8af302a89f47c22e1fdfc
readgovip <107192467+readgovip@users.noreply.github.com>
2025-01-02 09:56:28 +0800

    Update 3xui-1.sh

--- 3xui-1.sh
+++ 3xui-1.sh
@@ -1,1 +1,55 @@
 
+#!/bin/bash
+red='\033[0;31m'
+green='\033[0;32m'
+blue='\033[0;34m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+# check root
+[[ $EUID -ne 0 ]] && echo -e "${red}Fatal error: ${plain} Please run this script with root privilege \n " && exit 1
+
+# Check OS and set release variable
+if [[ -f /etc/os-release ]]; then
+    source /etc/os-release
+    release=$ID
+elif [[ -f /usr/lib/os-release ]]; then
+    source /usr/lib/os-release
+    release=$ID
+else
+    echo "Failed to check the system OS, please contact the author!" >&2
+    exit 1
+fi
+echo "The OS release is: $release"
+
+arch() {
+    case "$(uname -m)" in
+    x86_64 | x64 | amd64) echo 'amd64' ;;
+    i*86 | x86) echo '386' ;;
+    armv8* | armv8 | arm64 | aarch64) echo 'arm64' ;;
+    armv7* | armv7 | arm) echo 'armv7' ;;
+    armv6* | armv6) echo 'armv6' ;;
+    armv5* | armv5) echo 'armv5' ;;
+    s390x) echo 's390x' ;;
+    *) echo -e "${green}Unsupported CPU architecture! ${plain}" && rm -f 3xui.sh && exit 1 ;;
+    esac
+}
+echo "arch: $(arch)"
+
+# Install Docker
+docker -v
+if [ $? -ne  0 ]; then
+    curl -sSL https://get.docker.com | bash
+fi
+
+cd /opt && git clone https://github.com/Misaka-blog/3x-ui.git
+cd /opt/3x-ui
+docker run -itd \
+   -e XRAY_VMESS_AEAD_FORCED=false \
+   -v $PWD/db/:/etc/x-ui/ \
+   -v $PWD/cert/:/root/cert/ \
+   --network=host \
+   --restart=unless-stopped \
+   --name 3x-ui \
+   ghcr.io/misaka-blog/3x-ui:latest
+   
