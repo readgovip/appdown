02aca8abf8cf978817e80ed2fa59bb9397b1c30d
readgovip <107192467+readgovip@users.noreply.github.com>
2025-01-04 23:41:47 +0800

    Update 3xui-1.sh

--- 3xui-1.sh
+++ 3xui-1.sh
@@ -48,7 +48,6 @@ docker run -itd \
    -e XRAY_VMESS_AEAD_FORCED=false \
    -v $PWD/db/:/etc/x-ui/ \
    -v $PWD/cert/:/root/cert/ \
-   -p 19853:2053 \
    --network=host \
    --restart=unless-stopped \
    --name 3x-ui \
