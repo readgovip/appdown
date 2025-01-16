5bb894e9998526ca41a60aeae35fa446934d2815
readgovip <readgovip@gmail.com>
2025-01-17 03:09:53 +0800

    Update iport.sh

--- iport.sh
+++ iport.sh
@@ -30,7 +30,15 @@ case "$Action" in
 			RemotePort=$(echo $RemoteIp_Port | cut -d ':' -f 2)		
 			LocalPort=$3
 		fi
+		
+		# 检测端口是否存在
 		check_port_forwarding $LocalPort
+	    # 添加iptables规则
+		iptables -t nat -I PREROUTING -p tcp --dport "$LocalPort" -j DNAT --to "$RemoteIp":"$RemotePort"
+		iptables -t nat -I POSTROUTING -p tcp -d "$RemoteIp" --dport "$RemotePort" -j MASQUERADE
+		iptables -t nat -I PREROUTING -p udp --dport "$LocalPort" -j DNAT --to "$RemoteIp":"$RemotePort"
+		iptables -t nat -I POSTROUTING -p udp -d "$RemoteIp" --dport "$RemotePort" -j MASQUERADE	
+		echo "已添加端口转发: 从本地端口 $LocalPort 到 $RemoteIp:$RemotePort"
 		;;
 
    "del")
@@ -40,21 +48,34 @@ case "$Action" in
 			exit 1
 		# 判断参数有2个
 		elif [[ "$#" -le 2 ]]; then
-		
+			# 提取RemoteIp和RemotePort
+			RemoteIp_Port=$2
+			RemoteIp=$(echo $RemoteIp_Port | cut -d ':' -f 1)
+			RemotePort=$(echo $RemoteIp_Port | cut -d ':' -f 2)		
+			LocalPort=$RemotePort
 		# 判断参数有3个
 		else
-		
+			# 提取RemoteIp和RemotePort
+			RemoteIp_Port=$2
+			RemoteIp=$(echo $RemoteIp_Port | cut -d ':' -f 1)
+			RemotePort=$(echo $RemoteIp_Port | cut -d ':' -f 2)		
+			LocalPort=$3		
 		fi
+		
+		# 删除iptables规则
+		iptables -t nat -D PREROUTING -p tcp --dport "$LocalPort" -j DNAT --to "$RemoteIp":"$RemotePort"
+		iptables -t nat -D POSTROUTING -p tcp -d "$RemoteIp" --dport "$RemotePort" -j MASQUERADE
+		iptables -t nat -D PREROUTING -p udp --dport "$LocalPort" -j DNAT --to "$RemoteIp":"$RemotePort"
+		iptables -t nat -D POSTROUTING -p udp -d "$RemoteIp" --dport "$RemotePort" -j MASQUERADE	
+		echo "已删除端口转发: 从本地端口 $LocalPort 到 $RemoteIp:$RemotePort"		
 		;;
    
    "info")
-		echo "淘宝网"
+		echo "##############################当前iptables配置##############################"
+        iptables -L PREROUTING -n -t nat --line-number
+        iptables -L POSTROUTING -n -t nat --line-number
 		;;
 esac
-
-
-
-
 
 # 检测端口转发规则的函数
 check_port_forwarding() {
@@ -71,25 +92,3 @@ check_port_forwarding() {
 		exit 1
     fi
 }
-
-
-
-# 根据操作选择添加或删除规则
-if [[ "$Action" == "add" ]]; then
-    # 添加iptables规则
-    iptables -t nat -I PREROUTING -p tcp --dport "$LocalPort" -j DNAT --to "$RemoteIp":"$RemotePort"
-    iptables -t nat -I POSTROUTING -p tcp -d "$RemoteIp" --dport "$RemotePort" -j MASQUERADE
-    iptables -t nat -I PREROUTING -p udp --dport "$LocalPort" -j DNAT --to "$RemoteIp":"$RemotePort"
-    iptables -t nat -I POSTROUTING -p udp -d "$RemoteIp" --dport "$RemotePort" -j MASQUERADE	
-    echo "已添加端口转发: 从本地端口 $LocalPort 到 $RemoteIp:$RemotePort"
-elif [[ "$Action" == "del" ]]; then
-    # 删除iptables规则
-    iptables -t nat -D PREROUTING -p tcp --dport "$LocalPort" -j DNAT --to "$RemoteIp":"$RemotePort"
-    iptables -t nat -D POSTROUTING -p tcp -d "$RemoteIp" --dport "$RemotePort" -j MASQUERADE
-    iptables -t nat -D PREROUTING -p udp --dport "$LocalPort" -j DNAT --to "$RemoteIp":"$RemotePort"
-    iptables -t nat -D POSTROUTING -p udp -d "$RemoteIp" --dport "$RemotePort" -j MASQUERADE	
-    echo "已删除端口转发: 从本地端口 $LocalPort 到 $RemoteIp:$RemotePort"
-else
-    echo "无效操作: $1。请使用 'Add' 或 'Del'。"
-    exit 1
-fi
